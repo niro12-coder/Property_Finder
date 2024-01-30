322d2a18849359a51b95ea7b13cbbc9b9907a21a
farahl0 <116227197+farahl0@users.noreply.github.com>
2024-01-30 16:25:19 +0200

    gg

---
+++ src/main/java/com/example/propertyfinder/HelloController.java
@@ -0,0 +1,11 @@
+package com.example.propertyfinder;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class HelloController {
+
+    @FXML
+    private ImageView image;
+
+}
