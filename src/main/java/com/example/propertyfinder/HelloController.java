bdb88eee0f63af730b947b513288c2a556b40a5d
farahl0 <116227197+farahl0@users.noreply.github.com>
2024-02-01 21:10:05 +0200

    fofo

--- src/main/java/com/example/propertyfinder/HelloController.java
+++ src/main/java/com/example/propertyfinder/HelloController.java
@@ -1,11 +1,27 @@
 package com.example.propertyfinder;
 
 import javafx.fxml.FXML;
-import javafx.scene.image.ImageView;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
 
     @FXML
-    private ImageView image;
+    private Button register;
+
+    @FXML
+    void registeracc(MouseEvent event) throws IOException {
+        Stage stage =(Stage) register.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("register.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Property Finder");
+        stage.setScene(scene);
+
+    }
 
 }
